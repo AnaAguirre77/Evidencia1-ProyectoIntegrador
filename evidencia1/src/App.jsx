599457ead9a296a17c9dedb68de62557c911d6bc
AnaAguirre77 <anaaguirre219@gmail.com>
2025-05-14 23:21:12 -0300

    Actividad 3, trabajando con Axios

--- evidencia1/src/App.jsx
+++ evidencia1/src/App.jsx
@@ -1,13 +1,34 @@
 import "./App.css";
 
-function App() {
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Productos = () => {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((res) => setProductos(res.data.products))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <div className="p-8">
-      <h1 className="text-3xl text-purple-600 font-bold">
-        Hola! Soy un título violeta.
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-center mb-6 text-pink-500">
+        Actividad 3. Pidiendo una lista de productos, con Axios.
       </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+        {productos.map((producto) => (
+          <div key={producto.id} className="border p-4 rounded shadow">
+            <h2 className="font-bold">{producto.title}</h2>
+            <p>${producto.price}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Productos;
